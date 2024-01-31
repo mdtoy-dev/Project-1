1e276f2b15e2e593b59cb93fc7ae956d8420c632
Thinh <thinh1198@hotmail.com>
2024-01-31 21:37:33 +0000

    first commit - old file

---
+++ scripts.js
@@ -0,0 +1,12 @@
+apiKey = 'IZ3Jet89Cn5Z6VkL7lFYgkqB7TJ4XM9h';
+
+
+queryURL = `https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=UK&year=2019`;
+
+fetch(queryURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
