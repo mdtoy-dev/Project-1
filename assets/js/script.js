b40e7f22dc06ca309bb8b597eaa791e3d4ac8b4e
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-02-01 20:28:33 +0000

    logs holiday names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,47 @@
-var apikey = "XvFWChUAog5bDCASmYccuidsOvVPlSns"
-var country = "GB"
-var year = 2024
-var queryURL = "https://calendarific.com/api/v2/holidays?&api_key=" +  apikey  + "&country=" + country +"&year=" + year;
+var apikey = "XvFWChUAog5bDCASmYccuidsOvVPlSns";
+var country = "GB";
+var year = 2024;
+var queryURL =
+  "https://calendarific.com/api/v2/holidays?&api_key=" +
+  apikey +
+  "&country=" +
+  country +
+  "&year=" +
+  year;
 
-fetch(queryURL)
-    .then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data);
+function getData() {
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (data) {
+      console.log(data);
+      return data;
+    });
+}
 
-var titles = "New_Year's_Day"
+function addButtons(inputObject) {
+  var holidays = inputObject.response.holidays;
+  console.log(holidays);
+  var main = document.querySelector("main");
+  for (let i = 0; i < holidays.length; i++) {
+    var dayObject = holidays[i];
+    var holidayName = dayObject.name;
+    console.log(holidayName);
+    // main.appendChild();
+  }
+}
+
+var dataObject = getData();
+addButtons(dataObject);
+
+var titles = "New_Year's_Day";
 var wikiQueryURL = `https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${titles}&origin=*&formatversion=2`;
 
 fetch(wikiQueryURL)
-    .then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data)
-    })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
