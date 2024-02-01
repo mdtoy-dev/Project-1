96c3f9fc10eb8c784e5c98b2b5e3dac5cc2f29d1
Thinh <thinh1198@hotmail.com>
2024-02-01 21:12:27 +0000

    countries options are now displayed in alphabetic section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+
 // fetch countries and flags.
 apiKey = 'IZ3Jet89Cn5Z6VkL7lFYgkqB7TJ4XM9h'
 queryURL = `https://calendarific.com/api/v2/countries?api_key=${apiKey}`
@@ -8,19 +9,30 @@ fetch(queryURL)
         return response.json();
     })
     .then(function(data) {
-        $('#countryGrid > fieldset').empty();
-        for (var i = 0; i < data.response.countries.length; i++) {
-            $('#countryGrid > fieldset').append(
-                $('<button>').addClass('country-btn').attr({'data-bs-dismiss': 'modal'}).html(
-                    `${`<img src="https://flagcdn.com/${(data.response.countries[i]['iso-3166']).toLowerCase()}.svg" width="24" alt="${data.response.countries[i].country_name}">`} ${data.response.countries[i].country_name}`)
-            )
+
+        $('#countryGrid').empty();
+        var alphabet = [...('abcdefghijklmnopqrstuvwyz').toUpperCase()];
+        for (var i = 0; i < alphabet.length; i++) {
+            $('#countryGrid').append($('<div>').addClass('country-items')
+                                .append($('<h4>').addClass(`header-${alphabet[i]}`).text(alphabet[i]), $('<div>').addClass(`country-alphabet`).attr({id: `country-${alphabet[i]}`, 'data-id': `${alphabet[i]}`}), $('<hr>')))
+        }
+
+        for (var i = 0; i < alphabet.length; i++) {
+            var getLetter = $(`#country-${alphabet[i]}`).attr('data-id');
+            for (var j = 0; j < data.response.countries.length; j++) {
+                var getCity = data.response.countries[j].country_name[0];
+                if (getLetter === getCity) {
+                    $(`#country-${alphabet[i]}`).append($('<button>').addClass('country-btn').attr({'data-bs-dismiss': 'modal'}).html(
+                        `${`<img src="https://flagcdn.com/${(data.response.countries[j]['iso-3166']).toLowerCase()}.svg" width="24" alt="${data.response.countries[j].country_name}">`} ${data.response.countries[j].country_name}`)
+                    )
+                }
+            }
         }
     })
 
 // toggles country modal.
 $('.country-btn').on('click', function(event) {
     event.preventDefault();
-    // getCountry();
 })
 
 // response for countries button click.
