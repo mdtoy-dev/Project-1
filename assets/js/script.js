5cf5d21f4a5a6c188a21720f65a3082468276eb5
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-02-01 20:45:24 +0000

    appended holiday names to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,31 +9,25 @@ var queryURL =
   "&year=" +
   year;
 
-function getData() {
-  fetch(queryURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      return data;
-    });
-}
-
-function addButtons(inputObject) {
-  var holidays = inputObject.response.holidays;
-  console.log(holidays);
-  var main = document.querySelector("main");
-  for (let i = 0; i < holidays.length; i++) {
-    var dayObject = holidays[i];
-    var holidayName = dayObject.name;
-    console.log(holidayName);
-    // main.appendChild();
-  }
-}
-
-var dataObject = getData();
-addButtons(dataObject);
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var holidays = data.response.holidays;
+    console.log(holidays);
+    var main = document.querySelector(".test");
+    for (let i = 0; i < holidays.length; i++) {
+      var dayObject = holidays[i];
+      var holidayName = dayObject.name;
+      console.log(holidayName);
+      var pName = document.createElement("p");
+      pName.setAttribute("class", "my-2 mx-2");
+      pName.textContent = holidayName;
+      main.appendChild(pName);
+    }
+  });
 
 var titles = "New_Year's_Day";
 var wikiQueryURL = `https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${titles}&origin=*&formatversion=2`;
