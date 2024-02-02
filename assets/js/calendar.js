6adce98aebe22cd136e348a72c5285f79e498441
mdtoy-dev <berk.toy1@gmail.com>
2024-02-02 19:27:34 +0000

    calendar now can change days and display

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,8 +1,10 @@
 var currentDate = document.querySelector(".current-date")
 var days = document.querySelector(".days")
 var icons = document.querySelectorAll(".fa")
+var panelText = document.querySelector(".panel-text")
 //var months = ["January", "February", "March", "April", "May", "June" , "July", "August", "September", "October", "November", "December"]
 var date = dayjs()
+var newDay = ""
 
 function renderCalendar() {
     var daysInMonth = date.daysInMonth()
@@ -17,9 +19,9 @@ function renderCalendar() {
 
     for (var i = 1; i <= daysInMonth; i++) {
         if (i === dayjs().date() && date.month() === dayjs().month()) {
-            listDay += `<li class="active">${i}</li>`
+            listDay += `<li class="active" onclick="selectDay(${i}, this)">${i}</li>`
         } else {
-        listDay += `<li>${i}</li>`
+        listDay += `<li onclick="selectDay(${i}, this)">${i}</li>`
         }
     }
 
@@ -41,10 +43,22 @@ icons.forEach(function (icon, index) {
 
 function changeMonth(index) {
     if (icons[index].id === "left") {
-        date = date.subtract(1, 'month');
+        date = date.subtract(1, 'month')
     } else {
-        date = date.add(1, 'month');
+        date = date.add(1, 'month')
     }
 
     renderCalendar();
+}
+
+function selectDay(selectedDay, newDay) {
+
+    date = date.date(selectedDay)
+    if (document.querySelector(".active")) {
+        document.querySelector(".active").classList.remove("active")
+    } 
+    newDay.classList.add("active")
+    console.log(`Selected day: ${selectedDay}`)
+    console.log(date)
+    panelText.textContent = date
 }
