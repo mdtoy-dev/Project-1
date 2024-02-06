dd956e98b055f55bdccc2fc7c31f83a261deab34
Thinh <thinh1198@hotmail.com>
2024-02-06 18:26:47 +0000

    uncomment some function

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,10 +104,8 @@ $(document).on("click", '.country-option-btn, .year-option-btn', function (event
 });
 
 // render countries and flags.
-// let apiKey1 = 'IZ3Jet89Cn5Z6VkL7lFYgkqB7TJ4XM9h'
-// let apiKey1 = "XvFWChUAog5bDCASmYccuidsOvVPlSns";
-apiKeyT = 'Okaf0L9aadr7gnB1eemtZUMfwYHfEgw9'
-let queryURL1 = `https://calendarific.com/api/v2/countries?api_key=${apiKeyT}`;
+// apiKey1 = 'Okaf0L9aadr7gnB1eemtZUMfwYHfEgw9'
+// let queryURL1 = `https://calendarific.com/api/v2/countries?api_key=${apiKey1}`;
 
 // generate buttons of countries for user to pick.
 fetch(queryURL1)
@@ -156,61 +154,61 @@ fetch(queryURL1)
     }
   });
 
-// var apiKey2 = "XvFWChUAog5bDCASmYccuidsOvVPlSns";
-// var country = "GB";
-// var year = 2024;
-// var queryURL2 =
-//   "https://calendarific.com/api/v2/holidays?&api_key=" +
-//   apiKey1 +
-//   "&country=" +
-//   country +
-//   "&year=" +
-//   year;
+var apiKey2 = "XvFWChUAog5bDCASmYccuidsOvVPlSns";
+var country = "GB";
+var year = 2024;
+var queryURL2 =
+  "https://calendarific.com/api/v2/holidays?&api_key=" +
+  apiKey1 +
+  "&country=" +
+  country +
+  "&year=" +
+  year;
 
 // function to run when special day is clicked
-// function clickEvent(index, description) {
-//   //log arguments
-//   console.log("Clicked!");
-//   console.log("index: " + index);
-//   console.log("description: " + description);
-//   //update description p element with relevant description
-//   var pDescription = document.getElementById("infoDescription");
-//   var clickedId = "p" + index;
-//   var pClicked = document.getElementById(clickedId);
-//   pDescription.textContent = description;
-// }
+function clickEvent(index, description) {
+  //log arguments
+  console.log("Clicked!");
+  console.log("index: " + index);
+  console.log("description: " + description);
+  //update description p element with relevant description
+  var pDescription = document.getElementById("infoDescription");
+  var clickedId = "p" + index;
+  var pClicked = document.getElementById(clickedId);
+  pDescription.textContent = description;
+}
 
-// fetch(queryURL2)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//     //define description p element and set id and class attributes
-//     var pDescription = document.createElement("p");
-//     pDescription.setAttribute("id", "infoDescription");
-//     pDescription.setAttribute("class", "ms-auto my-2 me-2");
-//     var main = document.querySelector(".test");
-//     //add description p element to main
-//     main.appendChild(pDescription);
-//     var holidays = data.response.holidays;
-//     //create holiday name p tags and assign clickEvent function on click within for loop
-//     for (let i = 0; i < holidays.length; i++) {
-//       let dayObject = holidays[i];
-//       var holidayName = dayObject.name;
-//       let holidayDescription = dayObject.description;
-//       var pName = document.createElement("p");
-//       pName.setAttribute("class", "my-2 mx-2");
-//       var idToSet = "p" + i.toString();
-//       pName.setAttribute("id", idToSet);
-//       pName.textContent = holidayName;
-//       //pName is each p element with the holiday name as its text content
-//       pName.onclick = function () {
-//         //arguments are the index of the for loop used to find the id of the holiday name p element
-//         //and the description to update the description p element with
-//         clickEvent(i, holidayDescription);
-//       };
-//       //add holiday name p element to main
-//       main.appendChild(pName);
-//     }
-//   });
+fetch(queryURL2)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    //define description p element and set id and class attributes
+    var pDescription = document.createElement("p");
+    pDescription.setAttribute("id", "infoDescription");
+    pDescription.setAttribute("class", "ms-auto my-2 me-2");
+    var main = document.querySelector(".test");
+    //add description p element to main
+    main.appendChild(pDescription);
+    var holidays = data.response.holidays;
+    //create holiday name p tags and assign clickEvent function on click within for loop
+    for (let i = 0; i < holidays.length; i++) {
+      let dayObject = holidays[i];
+      var holidayName = dayObject.name;
+      let holidayDescription = dayObject.description;
+      var pName = document.createElement("p");
+      pName.setAttribute("class", "my-2 mx-2");
+      var idToSet = "p" + i.toString();
+      pName.setAttribute("id", idToSet);
+      pName.textContent = holidayName;
+      //pName is each p element with the holiday name as its text content
+      pName.onclick = function () {
+        //arguments are the index of the for loop used to find the id of the holiday name p element
+        //and the description to update the description p element with
+        clickEvent(i, holidayDescription);
+      };
+      //add holiday name p element to main
+      main.appendChild(pName);
+    }
+  });
