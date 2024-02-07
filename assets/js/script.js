90904718b9c470e3048f1edd4cf3540cc1421a43
mdtoy-dev <berk.toy1@gmail.com>
2024-02-07 20:14:06 +0000

    scriptjs file updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // let apiKey1 = 'IZ3Jet89Cn5Z6VkL7lFYgkqB7TJ4XM9h'
 // let apiKey1 = "XvFWChUAog5bDCASmYccuidsOvVPlSns";
 
-let apiKey1 = "Okaf0L9aadr7gnB1eemtZUMfwYHfEgw9";
+let calendarificAPI = "Okaf0L9aadr7gnB1eemtZUMfwYHfEgw9";
 
 // response to clicking user input buttons.
 $(document).on('click', '.country-btn', function(event) {
@@ -18,7 +18,7 @@ $(document).on("click", '#countryOptionBtn', async function(event) {
 });
 
 // render countries buttons with flags.
-let queryURL1 = `https://calendarific.com/api/v2/countries?api_key=${apiKey1}`;
+let queryURL1 = `https://calendarific.com/api/v2/countries?api_key=${calendarificAPI}`;
 fetch(queryURL1)
   .then(function (response) {
     return response.json();
@@ -67,119 +67,8 @@ fetch(queryURL1)
 });
 
 var countryCode = "";
-
-// response for countries button click.
-$(document).on("click", ".country-btn-select", function (event) {
-  event.preventDefault();
-  var $button = $(this);
-  // console.log($button);
-  // console.log($button.textContent);
-  console.log($button.attr("data-countryCode"));
-  countryCode = $button.attr("data-countryCode");
-  localStorage.setItem("countryCode", countryCode);
-});
-
-$("#toggle-modal-btn").on("click", function () {
-  localStorage.clear();
-});
-
 // var apiKey2 = "XvFWChUAog5bDCASmYccuidsOvVPlSns";
-var country = "";
-console.log("trying to retrieve country from local storage");
-try {
-  console.log("try block");
-  var retrievedCountry = localStorage.getItem("countryCode");
-  //check if retrievedCountry is falsey
-  if (!retrievedCountry) {
-    console.log("no country found in local storage, defaulting to GB");
-    country = "GB";
-  } else {
-    console.log("country in local storage is found");
-    console.log(retrievedCountry);
-    country = retrievedCountry;
-  }
-} catch (error) {
-  console.log("catch block");
-  console.log(
-    "error: " +
-      error +
-      "\nin retrieving country code from local storage. Defaulting to GB"
-  );
-  country = "GB";
-}
-
-console.log("country before query: " + country);
-
 var year = dayjs().format("YYYY");
-var queryURL2 =
-  "https://calendarific.com/api/v2/holidays?&api_key=" +
-  apiKey1 +
-  "&country=" +
-  country +
-  "&year=" +
-  year;
-
-// console.log(queryURL2);
-
-// function to run when special day is clicked
-function clickEvent(index, description) {
-  //update description p element with relevant description
-  var pDescription = document.getElementById("infoDescription");
-  var clickedId = "p" + index;
-  var pClicked = document.getElementById(clickedId);
-  pDescription.textContent = description;
-}
-
-fetch(queryURL2)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    //define description p element and set id and class attributes
-    var pDescription = document.createElement("p");
-    pDescription.setAttribute("id", "infoDescription");
-    pDescription.setAttribute("class", "ms-auto my-2 me-2 text-white");
-    var specialDay = document.getElementById("special-day");
-    // define event div for use in eventAppend function
-    var divEvent = document.createElement("div");
-    divEvent.setAttribute("id", "eventContainer");
-    divEvent.setAttribute("class", "panel-text text-light p-5");
-    specialDay.appendChild(pDescription);
-    specialDay.appendChild(divEvent);
-    var holidays = data.response.holidays;
-
-    setInterval(function () {
-      var dateInstance = date;
-      var month = dateInstance.$M;
-      //actively selected day is given the .active class in calendar.js
-      //get element that is actively selected and assign to variable
-      var selectedDay = document.querySelector(".active");
-
-      //if no day is selected clear the description p tag in info panel
-      if (selectedDay === null) {
-        pDescription.textContent = "";
-        return;
-      }
-
-      var holidayFound = false;
-      for (i = 0; i < data.response.holidays.length; i++) {
-        // get the holiday object
-        var dayObject = holidays[i];
-        //returns date object with numbers for day, month, year and assign to variable
-        var holidayDate = dayObject.date.datetime;
-        //gets actively selected day
-        var day = selectedDay.textContent;
-        if (holidayDate.month - 1 == month && holidayDate.day == day) {
-          clickEvent(i, dayObject.description);
-          holidayFound = true;
-        }
-      }
-      if (holidayFound === false) {
-        pDescription.textContent = "";
-      }
-    }, 500);
-  });
 
 function eventAppend() {
   // Ticketmaster Event API
@@ -220,4 +109,73 @@ function eventAppend() {
         }
       }
     });
+
+
+  var specialDaysQuery =
+  "https://calendarific.com/api/v2/holidays?&api_key=" +
+  calendarificAPI +
+  "&country=" +
+  countryCode +
+  "&year=" +
+  year;
+
+  console.log(specialDaysQuery);
+  
+  // function to run when special day is clicked
+  function clickEvent(index, description) {
+    //update description p element with relevant description
+    var pDescription = document.getElementById("infoDescription");
+    var clickedId = "p" + index;
+    //future use of click event for special day(not using currently)
+    //var pClicked = document.getElementById(clickedId);
+    pDescription.textContent = description;
+  }
+  
+  fetch(specialDaysQuery)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var pDescription = document.getElementById("infoDescription");
+      var holidays = data.response.holidays;
+  
+      var dateInstance = date;
+      var month = dateInstance.$M;
+      //actively selected day is given the .active class in calendar.js
+      //get element that is actively selected and assign to variable
+      var selectedDay = document.querySelector(".active");
+  
+      //if no day is selected clear the description p tag in info panel
+      if (selectedDay === null) {
+        pDescription.textContent = "";
+        return;
+      }
+  
+      var holidayFound = false;
+      for (i = 0; i < data.response.holidays.length; i++) {
+        // get the holiday object
+        var dayObject = holidays[i];
+        //returns date object with numbers for day, month, year and assign to variable
+        var holidayDate = dayObject.date.datetime;
+        //gets actively selected day
+        var day = selectedDay.textContent;
+        if (holidayDate.month - 1 == month && holidayDate.day == day) {
+          clickEvent(i, dayObject.description);
+          holidayFound = true;
+        }
+      }
+      if (holidayFound === false) {
+        pDescription.textContent = "";
+      }
+      
+    });
+  
+
+
+
+
+
+
+
 }
