950351e5453888aef8ea031c33c487d147239ff2
Fiza Pari <arshiyashahir3@gmail.com>
2024-02-07 22:16:45 +0000

    notes code has updated

--- assets/js/note.js
+++ assets/js/note.js
@@ -7,14 +7,18 @@ days.addEventListener("click", function(event){
     console.log("this is days", days)
     console.log("this is event", event)
     if(event.target.tagName==="LI"){
-// const number=event.target.textContent.trim()
-// inputBox.value=number
+
      inputBox.style.display="block"
     }
 });
 inputBox.addEventListener("input",function(event){
+    
     const userValue=event.target.value
     console.log(userValue)
-} )
+    localStorage.setItem("userKey", userValue)
+    const noteValue1=document.getElementById("noteValue");
+console.log(noteValue1)
+noteValue1.textContent=localStorage.getItem("userKey")
+})
 
 })
